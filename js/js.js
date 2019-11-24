d1f6fadbb7c88ed43a4b531ee11e0fa10c92f5d7
nengahadinda27 <nengah.adinda27@gmail.com>
2019-11-24 20:01:56 +0700

    add-ubah-berita-v1

--- js/js.js
+++ js/js.js
@@ -30,8 +30,8 @@ function bacaGambar(input) {
     }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// tambah Berita
+var modal = document.getElementById("tambahBerita");
 
 // Get the button that opens the modal
 var btn = document.getElementById("tambah");
@@ -53,5 +53,31 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+// Detail Berita
+var modal1 = document.getElementById("detailBerita");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("detail");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal1.style.display = "none";
   }
 }

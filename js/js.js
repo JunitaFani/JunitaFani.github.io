f7f645fe7f3c109f7cefc577e0c37e564a673c47
nengahadinda27 <nengah.adinda27@gmail.com>
2019-12-01 20:24:44 +0700

    popup-login

--- js/js.js
+++ js/js.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-    
     var readURL = function(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -13,7 +11,6 @@ $(document).ready(function() {
         }
     }
     
-
     $(".file-upload").on('change', function(){
         readURL(this);
     });
@@ -59,25 +56,43 @@ window.onclick = function(event) {
 // Detail Berita
 var modal1 = document.getElementById("detailBerita");
 
-// Get the button that opens the modal
+
 var btn1 = document.getElementById("detail");
 
-// Get the <span> element that closes the modal
+
 var span1 = document.getElementsByClassName("close1")[0];
 
-// When the user clicks on the button, open the modal
+
 btn1.onclick = function() {
   modal1.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span1.onclick = function() {
   modal1.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal1.style.display = "none";
+  }
+}
+
+function validateForm() {
+  var username = document.forms["myForm"]["username"].value;
+  var password = document.forms["myForm"]["password"].value;
+  var a =0;
+  var b = 0;
+
+
+  if (username == "" || password == "") {
+    if(a == 0){
+        document.getElementById("popup-peringatan").style.display="block";
+        a = 1;
+    }
+
+    return false;
+
   }
 }

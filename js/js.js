56c938546921d3dbc104134ca1f07e2861b469a1
nengahadinda27 <nengah.adinda27@gmail.com>
2019-12-18 07:11:54 +0700

    fix 2

--- js/js.js
+++ js/js.js
@@ -54,6 +54,33 @@ window.onclick = function(event) {
   }
 }
 
+// ubah Banner
+var modal1 = document.getElementById("ubahBanner");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("ubah");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+   
+  }
+}
+
 
 
 function validateForm() {
